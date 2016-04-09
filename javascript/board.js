347296d44ec744b23fcc96b7b5ee81f40b0393dd
amaurs <amaury.gtz@gmail.com>
2016-04-08 19:31:20 -0500

    Added missing files from the last commit, javascript.

---
+++ javascript/board.js
@@ -0,0 +1,58 @@
+var board;
+var COLUMNS = 10;
+var ROWS = 5;
+
+function drawBoard()
+{
+	console.log("Hello World!");
+	board = createCleanBoard();
+	printBoardToElement(board, "container");
+
+}
+
+/**
+This function renders the given in a html tbody element.
+**/
+function printBoardToElement(board, element)
+{
+    console.log(board);
+    var row = "";
+    var container = document.getElementById("container");
+    for(var i = 0; i < board.length; i++)
+    {
+        var row = document.createElement('div');
+        for(var j = 0; j < board[0].length; j++)
+        {
+        	var column = document.createElement('div');
+        	row.appendChild(column);
+        }
+        container.appendChild(row);
+    }
+}
+
+/**
+Creates a zero filled array with the dimensions of regular tetris board.
+**/
+function createCleanBoard()
+{
+    return createZerosGrid(COLUMNS, ROWS);
+}
+
+/**
+Creates a zero filled array with given dimensions.
+**/
+function createZerosGrid(width, height)
+{
+    var arr = [];
+    for(var i = 0; i < height; i++)
+    {
+        var row = [];
+        for(var j = 0; j < width; j++)
+        {
+            row.push(0);
+        }
+
+        arr.push(row);
+    }
+    return arr;
+}
