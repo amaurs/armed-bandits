6a3f41de5f32de12c355fd5624958541d456a0c0
amaurs <amaury.gtz@gmail.com>
2016-04-12 00:38:11 -0500

    Added comments to the vector class.

--- javascript/vector.js
+++ javascript/vector.js
@@ -1,8 +1,15 @@
+/**
+A vector is used to represent a position inside de grid.
+**/
 function Vector(x, y) {
   this.x = x;
   this.y = y;
 }
 
-Vector.prototype.plus = function(other) {
+/**
+Performs a simple arithmetic operation over the vector. It adds two vectors pointwise.
+**/
+Vector.prototype.plus = function(other) 
+{
   return new Vector(this.x + other.x, this.y + other.y);
 };
